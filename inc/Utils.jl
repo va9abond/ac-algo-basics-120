91c778c07b6b806a6ccf3fc7be6fb5a13b2aa94f
rustem sirazetdinov <avesirazetdinov@gmail.com>
2025-05-04 02:07:48 +0300

    wip: add swap(v, i, j)

--- inc/Utils.jl
+++ inc/Utils.jl
@@ -6,3 +6,13 @@ function send_debug_info(io, msg...)
     debug_info_prefix = "[DEBUG] "
     println(io, debug_info_prefix, msg...)
 end
+
+@inline function swap(v, i, j)
+    @inbounds begin
+        temp = v[i]
+        v[i] = v[j]
+        v[j] = temp
+    end
+
+    return nothing
+end
