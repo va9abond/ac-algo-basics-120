d20b3b7463f8d8854eb2bdab0a8d3995817acb72
rustem sirazetdinov <avesirazetdinov@gmail.com>
2025-05-04 02:08:40 +0300

    wip[pr-3]: fix counting sort

--- practice-3/counting_sort.jl
+++ practice-3/counting_sort.jl
@@ -1,38 +1,36 @@
 function counting_sort!(v::Vector{Int64})
     min = findmin(v)[1]
     max = findmax(v)[1]
-    size = max-min+1
+    size, shift = max-min+1, min
 
-    max_size = 1000
-    size > max_size && return nothing
+    MAX_SIZE = 200000
+    (size > MAX_SIZE) && return nothing
 
-    counters = zeros(Int64, size)
+    buffer = zeros(Int64, size)
     for val in v
-        counters[val] += 1
+        @inbounds buffer[val-shift+1] += 1
     end
 
+
+    # open("logs", "w") do log
+    #     println(log, sort(v))
+    #     println(log)
+    #     println(log, "min: $(min), max: $(max), size = $(size)")
+    #
+    #     for (val, count) in enumerate(buffer)
+    #         println(log, "[$(val)]: $(count)")
+    #     end
+    #
+    #     println(log)
+    # end
+
     i = firstindex(v)
-    for (j, counter) in enumerate(counters)
-        for k in 1:counter
-            v[i] = j
+    for (val, count) in enumerate(buffer)
+        for _ in 1:count
+            @inbounds v[i] = val + (shift-1)
             i += 1
         end
     end
 
     return v
-    # v  = [8, 0, 1, 3, 2, 7, 4, 5, 9, 6]
 end
-
-function counting_sort(v::Vector{Int64})
-    return counting_sort!(copy(v))
-end
-
-function main()
-    v  = [8, 9, 6, 11, 1, 3, 1, 2, 1, 7, 8, 4, 7, 5, 9, 6]
-    println(v)
-
-    println()
-
-    println(counting_sort(v))
-end
-
